c2a050c3861f25f4a7fb09eaf10ffc891f881f01
Tamim-Shakil <tamim720sk@gmail.com>
2024-09-08 23:54:39 +0600

    Done

---
+++ basic.js
@@ -0,0 +1,52 @@
+// Rules of naming variables
+// 1. first we should define variable with "var"
+// 2. first letter should be smaller
+// 3. writing variales in camel case or snake case
+// 4. name should be meaningful (avoid using , x, y, z, a, b etc)
+// we can't use a number begining of a variable
+var number1 = 40;
+var number2 = 50;
+var $name = "Shakil";
+var firstName = "Tamim";
+var carModel = "2034";
+
+// Special Case
+var API_KEY = "asdflkjhdgfhfhg254dhfgrjhchrs";
+
+// Shake Case
+var first_name_of_footballer = "messi";
+var programming_language = "javascript";
+
+//   console.log(programming_language);
+//   console.log(number1);
+//   console.log($name);
+
+// write a program where you will able to find the sum, division, subtraction, multiplication of two numbers
+
+var numOne = 10;
+var numTwo = 2; // number
+var userName = "Tamim Shakil"; // string
+
+var addition = numOne + numTwo;
+var subtraction = numOne - numTwo;
+var multiplication = numOne * numTwo;
+var division = numOne / numTwo;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log(typeof "hello");
+console.log(typeof 30);
+
+//   var newType = userName + numOne; // string + number = string
+
+var numThree = 40;
+var numFour = "40";
+
+console.log(numThree * numFour);
+console.log(30 / "10");
+console.log(30 - "10");
+console.log("10" + 10);
+// DATA TYPES
+//number & string
